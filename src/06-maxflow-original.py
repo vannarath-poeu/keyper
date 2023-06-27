2d05f499c0f1257557a3e093af4062c745bc5d1c
Vannarath Poeu <vannarath.poeu@gmail.com>
2023-06-27 11:48:25 +0800

    added metrics

--- src/06-maxflow-original.py
+++ src/06-maxflow-original.py
@@ -38,6 +38,10 @@ def max_flow(
     assert os.path.exists(prediction_file_path), f"File {prediction_file_path} does not exist"
     assert os.path.exists(similarity_file_path), f"File {similarity_file_path} does not exist"
 
+    test_jsonl = f"data/midas/ldkp3k/test.jsonl"
+
+    assert os.path.exists(test_jsonl), f"File {test_jsonl} does not exist"
+
     if not os.path.exists(f"{output_path}"):
         pathlib.Path(f"{output_path}").mkdir(parents=True, exist_ok=True)
     
@@ -50,6 +54,20 @@ def max_flow(
 
     new_predictions = []
 
+    # with open(test_jsonl, "r") as f:
+    #     test = f.readlines()
+    # results = {
+    #     k : {
+    #         "precision@5": 0,
+    #         "recall@5": 0,
+    #         "fscore@5": 0,
+    #         "precision@10": 0,
+    #         "recall@10": 0,
+    #         "fscore@10": 0,
+    #     }
+    #     for k in ["abstractive", "extractive", "combined"]
+    # }
+
     num_records = 1 #len(predictions)
     for i in range(num_records):
         model = pyo.ConcreteModel("max_flow")
@@ -60,6 +78,10 @@ def max_flow(
 
         section_keywords = [sk[:max_activation] for sk in predictions[i]]
         num_sections = len(section_keywords)
+
+        # test[i] = json.loads(test[i])
+        # abstractive_keyphrases = test[i]["abstractive_keyphrases"]
+        # extractive_keyphrases = test[i]["extractive_keyphrases"]
 
         # nodes
         nodes = set(["source", "sink"])
@@ -155,8 +177,40 @@ def max_flow(
         # predicted_keyphrases = sorted(keyword_scores, key=keyword_scores.get, reverse=True)[:10]
         predicted_keyphrases = rank_keywords(keyword_scores, top_n=10)
         new_predictions.append(predicted_keyphrases)
+
+        # abstractive_keyphrases = stem_keywords(abstractive_keyphrases)
+        # extractive_keyphrases = stem_keywords(extractive_keyphrases)
+        # combined_keyphrases = abstractive_keyphrases + extractive_keyphrases
+
+        # predicted_keyphrases = stem_keywords(predicted_keyphrases)
+
+        # for k in [5, 10]:
+        #     p, r, f = evaluate(predicted_keyphrases[:k], abstractive_keyphrases)
+        #     results["abstractive"][f"precision@{k}"] += p
+        #     results["abstractive"][f"recall@{k}"] += r
+        #     results["abstractive"][f"fscore@{k}"] += f
+
+        # for k in [5, 10]:
+        #     p, r, f = evaluate(predicted_keyphrases[:k], extractive_keyphrases)
+        #     results["extractive"][f"precision@{k}"] += p
+        #     results["extractive"][f"recall@{k}"] += r
+        #     results["extractive"][f"fscore@{k}"] += f
+
+        # for k in [5, 10]:
+        #     p, r, f = evaluate(predicted_keyphrases[:k], combined_keyphrases)
+        #     results["combined"][f"precision@{k}"] += p
+        #     results["combined"][f"recall@{k}"] += r
+        #     results["combined"][f"fscore@{k}"] += f
+        
         print(f"Processed {i+1} documents", end="\r")
 
+        # temp = copy.deepcopy(results)
+
+        # for k in temp.keys():
+        #     for score in temp[k].keys():
+        #         temp[k][score] /= (i+1)
+        # temp["num_docs"] = i+1
+        # json.dump(temp, open(f"{output_path}/scores.json", "w"), indent=4)
         json.dump(new_predictions, open(f"{output_path}/predictions.json", "w"), indent=4)
 
 
